ef0f62aa99f3dd9761dbcbe678331271aaeef3d3
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-06-16 12:24:57 +0900

    Update controller.py

--- scripts/controller.py
+++ scripts/controller.py
@@ -11,6 +11,7 @@ from std_srvs.srv import EmptyResponse
 import sys
 
 rospy.init_node('process_manager')
-subprocess.run('shell=True)
-               # rosrun teleop_twist_keyboard teleop_twist_keyboard.py _key_timeout:=0.6 _speed:=0.2 _turn:=0.2 cmd_vel:=/cmd_vel_raw
+subprocess.run('gnome-terminal -- bash -c "cd $HOME/catkin_ws; source /opt/ros/melodic/setup.bash; source $HOME/catkin_ws/devel/setup.bash; rosrun teleop_twist_keyboard teleop_twist_keyboard.py _key_timeout:=0.6 _speed:=0.2 _turn:=0.2 cmd_vel:=/cmd_vel_move; exec bash"'shell=True)
+subprocess.run('gnome-terminal -- bash -c "cd $HOME/catkin_ws; source /opt/ros/melodic/setup.bash; source $HOME/catkin_ws/devel/setup.bash; rosrun teleop_twist_keyboard teleop_twist_keyboard.py _key_timeout:=0.6 _speed:=0.2 _turn:=0.2 cmd_vel:=/cmd_vel_posture; exec bash"'shell=True)
+
 rospy.spin()

e1af180d4415cc41bc92a58b568937099df1788b
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-06-16 14:21:25 +0900

    Update control_mode.cpp

--- unitree_legged_real/src/exe/control_mode.cpp
+++ unitree_legged_real/src/exe/control_mode.cpp
@@ -100,7 +100,7 @@ template<typename TCmd, typename TState, typename TLCM>
 int mainHelper(int argc, char *argv[], TLCM &roslcm)
 {
     std::cout << "WARNING: Control level is set to HIGH-level." << std::endl
-              << "Make sure the robot is standing on the ground." << std::endl
+              << "Make sure the robot is standing on the ground." << std::endl;
 
     ros::NodeHandle n;
     ros::Rate loop_rate(500);

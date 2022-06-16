0dad9e356c8701bb3b460c8ed631452934c07e0e
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-06-16 12:23:03 +0900

    Update controller.py

--- scripts/controller.py
+++ scripts/controller.py
@@ -12,4 +12,5 @@ import sys
 
 rospy.init_node('process_manager')
 subprocess.run('shell=True)
+               # rosrun teleop_twist_keyboard teleop_twist_keyboard.py _key_timeout:=0.6 _speed:=0.2 _turn:=0.2 cmd_vel:=/cmd_vel_raw
 rospy.spin()

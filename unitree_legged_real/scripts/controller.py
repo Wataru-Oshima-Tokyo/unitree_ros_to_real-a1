bbb2386db4afd283e10909a5ff58d3933a80fead
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2022-06-16 12:35:58 +0900

    Update controller.py

--- unitree_legged_real/scripts/controller.py
+++ unitree_legged_real/scripts/controller.py
@@ -13,6 +13,7 @@ import sys
 
 rospy.init_node('process_manager')
 subprocess.Popen('gnome-terminal -- bash -c "cd $HOME/catkin_ws; source /opt/ros/melodic/setup.bash; source $HOME/catkin_ws/devel/setup.bash; rosrun teleop_twist_keyboard teleop_twist_keyboard.py _key_timeout:=0.6 _speed:=0.2 _turn:=0.2 cmd_vel:=/cmd_vel_move; exec bash"', shell=True)
+time.sleep(3)
 subprocess.Popen('gnome-terminal -- bash -c "cd $HOME/catkin_ws; source /opt/ros/melodic/setup.bash; source $HOME/catkin_ws/devel/setup.bash; rosrun teleop_twist_keyboard teleop_twist_keyboard.py _key_timeout:=0.6 _speed:=0.2 _turn:=0.2 cmd_vel:=/cmd_vel_posture; exec bash"', shell=True)
 
 rospy.spin()
